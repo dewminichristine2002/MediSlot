743df093975b0dda2fe7dbb0c83db8d8e4dc0b01
Dewmini Christine <dewminichristine@gmail.com>
2025-09-27 10:44:35 +0530

    ip change

--- MediSlotApp/src/utils/getApiBaseUrl.js
+++ MediSlotApp/src/utils/getApiBaseUrl.js
@@ -1,11 +1,6 @@
 
 
-  if (Platform.OS === 'android') return 'http://10.0.2.2:5000';
-  if (Platform.OS === 'ios')     return 'http://localhost:5000';
-  return LAN; // web or fallback
-};
-*/
 
-export const getApiBaseUrl = () => 'http://10.118.200.210:5000'; // your laptop's LAN IP
+export const getApiBaseUrl = () => 'http://10.118.200.10:5000'; // your laptop's LAN IP
 //192.168.8.140
 //192.168.8.140--home
