8a6b41b301ebaa2892f6a3eb3dd43b771b48fa78
Dewmini Christine <dewminichristine@gmail.com>
2025-09-27 23:31:38 +0530

    Ip address change

--- MediSlotApp/src/api/config.js
+++ MediSlotApp/src/api/config.js
@@ -1,14 +1,1 @@
-import { Platform } from 'react-native';
-
-/*export const getApiBaseUrl = () => {
-  // Change this to your LAN IP when testing on a real device
-  //const LAN = 'http://YOUR_LAN_IP:5000'; 
-  const LAN = 'http://192.168.8.156:5000'; 
-
-  if (Platform.OS === 'android') return 'http://10.0.2.2:5000';
-  if (Platform.OS === 'ios')     return 'http://localhost:5000';
-  return LAN; // web or fallback
-};
-*/
-
-export const getApiBaseUrl = () => 'http://10.118.200.10:5000'; // your laptop's LAN IP
+export const getApiBaseUrl = () => 'http://192.168.1.250:5000'; // your laptop's LAN IP
--- MediSlotApp/src/utils/getApiBaseUrl.js
+++ MediSlotApp/src/utils/getApiBaseUrl.js
@@ -1,3 +1,3 @@
 
 
-export const getApiBaseUrl = () => 'http://10.118.200.10:5000'; // your laptop's LAN IP
+export const getApiBaseUrl = () => 'http://192.168.1.250:5000'; // your laptop's LAN IP
