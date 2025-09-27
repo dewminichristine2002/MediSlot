b66aed54e43da1d1afe77582aaafa6dd97ee80cf
SanujiSilva <sanujisandanima@gmail.com>
2025-09-27 11:16:55 +0530

    changed ip

--- MediSlotApp/src/utils/getApiBaseUrl.js
+++ MediSlotApp/src/utils/getApiBaseUrl.js
@@ -1,11 +1,4 @@
 
-
-  if (Platform.OS === 'android') return 'http://10.0.2.2:5000';
-  if (Platform.OS === 'ios')     return 'http://localhost:5000';
-  return LAN; // web or fallback
-};
-*/
-
 export const getApiBaseUrl = () => 'http://10.118.200.210:5000'; // your laptop's LAN IP
 //192.168.8.140
 //192.168.8.140--home
