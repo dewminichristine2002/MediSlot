61bca716ba486bb3fd3a9b83a0bd8a7f94ecb4c4
Thyaga Alwis <thyagaalwis@gmail.com>
2025-09-27 11:37:48 +0530

    ip address

--- MediSlotApp/src/api/config.js
+++ MediSlotApp/src/api/config.js
@@ -1,14 +1,4 @@
 import { Platform } from 'react-native';
 
-/*export const getApiBaseUrl = () => {
-  // Change this to your LAN IP when testing on a real device
-  //const LAN = 'http://YOUR_LAN_IP:5000'; 
-  const LAN = 'http://192.168.8.156:5000'; 
-
-  if (Platform.OS === 'android') return 'http://10.0.2.2:5000';
-  if (Platform.OS === 'ios')     return 'http://localhost:5000';
-  return LAN; // web or fallback
-};
-*/
 
 export const getApiBaseUrl = () => 'http://10.114.128.1:5000'; // your laptop's LAN IP
