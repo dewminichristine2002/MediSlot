30549f40e67a6c65efd5aee5ffb72afcb92eb41d
SanujiSilva <sanujisandanima@gmail.com>
2025-10-07 14:55:05 +0530

    add health center names to event registration

--- MediSlotApp/src/utils/getApiBaseUrl.js
+++ MediSlotApp/src/utils/getApiBaseUrl.js
@@ -2,7 +2,7 @@
 
 
 
-export const getApiBaseUrl = () => 'http://10.118.200.210:5000'; // your laptop's LAN IP
+export const getApiBaseUrl = () => 'http://192.168.8.140:5000'; // your laptop's LAN IP
 
 //192.168.8.140
 //192.168.8.140--home
